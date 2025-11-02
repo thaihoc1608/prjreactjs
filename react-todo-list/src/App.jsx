b56132c506363a387f1761fbbea3490442334850
thaihoc1608 <hocnguyen830@gmail.com>
2025-11-02 09:58:37 +0700

    Sử dụng useState, tao todolist them cong viec

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+import Header from "./components/Header";
+
+
 function App() {
+  const [todoName, setTodoName] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function handleChangeInput(event) {
+    setTodoName(event.target.value);
+  }
+
+  function handleAddTask() {
+    const newTodo = {
+      id: new Date().getTime(),
+      name: todoName,
+    }
+    const newTodoList = [...todoList, newTodo];
+    setTodoList(newTodoList);
+    setTodoName("");
+  }
+
   return (
     <>
-      <h1>
-        Hello World!
-      </h1>
-      <p>Đây là JSX</p>
+      <Header />
+      <div className="add-form">
+        <input
+          type="text"
+          value={todoName}
+          onChange={handleChangeInput}
+          placeholder="Nhập công việc..."
+        />
+        <button onClick={handleAddTask}>Thêm công việc</button>
+      </div>
+
+      <div className="todo-list">
+        <h2>Danh sach viec can lam</h2>
+        {todoList.map((task) => (
+          <li key={task.id}>
+            {task.name}
+          </li>
+        ))}
+      </div>
     </>
   );
 }
---
+++ react-todo-list/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header>
+      <h1>Ứng dụng Todo List</h1>
+    </header>
+  );
+}
+export default Header;
